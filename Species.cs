9a9e69bee447ad6d598c63ed850ed0a1dcb65baa
Laure90 <58706312+Laure90@users.noreply.github.com>
2020-03-11 21:58:53 +0100

    Update Species.cs

--- Species.cs
+++ Species.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,5 +9,6 @@ namespace LINQ
         public Guid SpeciesID { get; set; }
         public String Name { get; set; }
         public int Population { get; set; }
+        public Animal Animal { get; set; }
     }
 }

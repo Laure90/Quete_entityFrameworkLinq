42a7ec00f657c681d5caec858fc4a77a5567d84d
Laure90 <58706312+Laure90@users.noreply.github.com>
2020-03-11 21:58:21 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,17 +13,62 @@ namespace LINQ
                 context.Database.EnsureDeleted();
                 context.Database.EnsureCreated();
 
-                List<Species> speciesList = new List<Species>() {
-                new Species() { Name = "white cougars", Population = 3},
-                new Species() { Name = "white tigers", Population = 100},
-                new Species() { Name = "albino turtles", Population = 15},
-        };
+                var speciesCougar = new Species
+                {
+                    Name = "cougars",
+                    Population = 3
+                };
+                var speciesTiger = new Species
+                {
+                    Name = "tigers",
+                    Population = 100
+                };
+                var speciesTurtle = new Species
+                {
+                    Name = "turtles",
+                    Population = 15
+                };      
 
-                
-                var whiteCougars = ;
-                var whiteTigers = ;
-                var albinoTurtles = ;
-                
+                var animal1 = new Animal
+                {
+                    Name = "White Cougar",
+                    Species= speciesCougar
+                };
+
+                var animal2 = new Animal
+                {
+                    Name = "White Tiger",
+                    Species = speciesTiger
+                };
+
+                var animal3 = new Animal
+                {
+                    Name = "Albinos Turtle",
+                    Species = speciesTurtle
+                };
+                List<Species> SpeciesList = new List<Species> { speciesCougar, speciesTiger, speciesTurtle};
+                List<Animal> AnimalList = new List<Animal> { animal1, animal2, animal3 };
+
+                context.AddRange(SpeciesList);
+                context.AddRange(AnimalList);
+                context.SaveChanges();
+
+                //foreach(var animal in context.Species)
+                //{
+
+                //}
+
+                var whiteCougars = from spec in context.Species
+                                   where spec.Name == "cougars"
+                                   select new { spec.Name, spec.Population };
+
+                var whiteTigers = from spec in context.Species
+                                  where spec.Name == "tigers"
+                                  select new { spec.Name, spec.Population };
+
+                var albinosTurtle = from spec in context.Species
+                                   where spec.Name == "turtles"
+                                   select new { spec.Name, spec.Population };
             }
         }
     }
